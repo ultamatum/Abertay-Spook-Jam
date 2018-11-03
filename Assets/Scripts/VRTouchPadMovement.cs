405fe6a5ec7c0ab1a628884a0004c292a8ef5e1b
ultamatum <alexanderg0502@gmail.com>
2018-11-03 19:37:21 +0000

    VR Player no longer collides with walls

--- Assets/Scripts/VRTouchPadMovement.cs
+++ Assets/Scripts/VRTouchPadMovement.cs
@@ -12,23 +12,43 @@ public class VRTouchPadMovement : MonoBehaviour
     public Hand hand;
     public SteamVR_Action_Boolean touch;
     public SteamVR_Action_Vector2 move;
+    public GameObject colliderObj;
     public float movementSpeed = 5;
 
-	void Start ()
+    Collider collider;
+    Vector3 prevPosition;
+    GameObject[] walls;
+
+    void Start ()
     {
         if (hand == null)
             hand = this.GetComponent<Hand>();
+
+        if (collider == null)
+            collider = colliderObj.GetComponent<Collider>();
+
+        if (walls == null)
+            walls = GameObject.FindGameObjectsWithTag("Wall");
 	}
 	
 	void Update ()
     {
+
         if(touch.GetState(hand.handType))
         {
-            rig.position += (((transform.right * move.GetAxis(hand.handType).x + transform.forward * move.GetAxis(hand.handType).y) * movementSpeed) * Time.deltaTime);
-            rig.position = new Vector3(rig.position.x, 0, rig.position.z);
-            Debug.Log(rig.position);
-            Debug.Log(move.GetAxis(hand.handType));
-            Debug.Log(touch.GetStateDown(hand.handType));
+            Vector3 newPos = (((transform.right * move.GetAxis(hand.handType).x + transform.forward * move.GetAxis(hand.handType).y) * movementSpeed) * Time.deltaTime);
+            foreach (GameObject wall in walls)
+            {
+                if (collider.bounds.Intersects(wall.GetComponent<Collider>().bounds))
+                {
+                    rig.position = prevPosition;
+                    Debug.Log("COLLIDED");
+                    return;
+                }
+            }
+            rig.position += new Vector3(newPos.x, 0, newPos.z);
         }
+
+        prevPosition = rig.position;
     }
 }
